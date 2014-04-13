de1f9e08a31c56eb4d6d88af8921124ddb06235d
thiendv <thienuit@gmail.com>
2014-04-14 00:55:41 +0700

    fix send package note

--- Project/HoGo.Demo/ServiceAPILib/src/com/gso/serviceapilib/ServiceAction.java
+++ Project/HoGo.Demo/ServiceAPILib/src/com/gso/serviceapilib/ServiceAction.java
@@ -1,6 +1,8 @@
 package com.gso.serviceapilib;
 
 public enum ServiceAction {
-	ActionNone, ActionInitApp, ActionLogin, ActionLogout, ActionUpload, ActionCheckEncodeStatus, ActionEncode, ActionSend,ActionGetMyDocument
+	ActionNone, ActionInitApp, ActionLogin, ActionLogout, 
+	ActionUpload, ActionCheckEncodeStatus, ActionEncode, 
+	ActionSend,ActionGetMyDocument, ActionSendPackageNote
 
 }
